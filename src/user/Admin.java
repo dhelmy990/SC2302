4f4b2d2104ad32e4e93c8e01235cc0718ad58f76
Clara Woon <clarawyx80@gmail.com>
2025-03-23 20:26:33 +0800

    Admin

---
+++ src/user/Admin.java
@@ -0,0 +1,37 @@
+package user;
+import java.util.*;
+
+public class Admin extends User{
+    public Admin(String username, String email, String password) {
+        super(username, email, password);
+    }
+
+    @Override
+    public String getRole() {
+        return "Admin";
+    }
+
+    // Adds a new user to the system
+    public void addUser(List<User> users, User newUser) {
+        users.add(newUser);
+        System.out.println("User added: " + newUser.getUsername());
+    }
+
+    // Adds a new stall to the system, get stall name
+    public void addStall(List<Stall> stalls, Stall newStall) {
+        stalls.add(newStall);
+        System.out.println("Stall added: " + newStall.getStall());
+    }
+
+    // Updates stall details
+    public void editStall(Stall stall, String newName) {
+        stall.setName(newName);
+    }
+
+    // Displays all transactions recorded in the system
+    public void viewAllTransactions(List<Transaction> transactions) {
+        for (Transaction txn : transactions) {
+            txn.display();
+        }
+    }
+}
