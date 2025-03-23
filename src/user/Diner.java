5e645141c940ea56d39d6a621af71b6b36ae3f5b
Clara Woon <clarawyx80@gmail.com>
2025-03-23 20:26:54 +0800

    Diner

---
+++ src/user/Diner.java
@@ -0,0 +1,28 @@
+package user;
+import java.util.*;
+
+public class Diner extends User{
+    public Diner(String username, String email, String password) {
+        super(username, email, password);
+    }
+
+    @Override
+    public String getRole() {
+        return "Diner";
+    }
+
+    // Place order via OrderManager
+    public int placeOrder(OrderManager orderManager, String stallName, List<String> items) {
+        return orderManager.requestOrder(stallName, items);
+    }
+
+    // View order summary after payment
+    public void viewOrderSummary(OrderManager orderManager, String username) {
+        orderManager.displayOrderSummaryForUser(username);
+    }
+
+    // View past order history (for logged-in users)
+    public void viewOrderHistory(OrderManager orderManager, String username) {
+        orderManager.displayOrderHistoryForUser(username);
+    }
+}
