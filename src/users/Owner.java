192766e32ed2db676e2c6ddd72f600e57a71a079
suying01 <chongsuying2005@gmail.com>
2025-03-28 21:41:29 +0800

    Return Owner object

--- src/users/Owner.java
+++ src/users/Owner.java
@@ -5,15 +5,27 @@ import orders.*;
 
 public class Owner extends User{
     private Stall managedStall;
+    private static Map<Integer, Owner> owners = new HashMap<>(); // Static map to store all owners by ID
+    private int ownerId;
 
-    public Owner(String username, String email, String password, Stall stall) {
+    public Owner(int ownerid, String username, String email, String password, Stall stall) {
         super(username, email, password);
         this.managedStall = stall;
+        this.ownerId = ownerId;
+        owners.put(ownerId,this) // Add the owner to the static map
     }
 
     @Override
     public String getRole() {
         return "Owner";
+    }
+
+    public static Owner get(int ownerid) {
+        return owners.get(ownerid);
+    }
+
+    public int getOwnerId(){
+        return this.owenerId;
     }
 
     // Stall knows its own Inventory. Inventory can display can update items
