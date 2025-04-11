ab178811c7f484c9f5e01f83ec84c6ccc929f919
Kelvin Lim Dao Quan <kelvinlimdq@gmail.com>
2025-04-11 08:35:09 +0000

    remove ordermanager method and queuemanger file

--- src/main/java/orders/OrderManager.java
+++ src/main/java/orders/OrderManager.java
@@ -6,16 +6,6 @@ import java.util.List;
 public class OrderManager {
     private final List<Order> orders = new ArrayList<>();
 
-    public List<Order> getOrdersByUser(String userId) {
-        List<Order> result = new ArrayList<>();
-        for (Order order : orders) {
-            if (order.getUsername().equals(userId)) {
-                result.add(order);
-            }
-        }
-        return result;
-    }
-
     public void addOrder(Order order) {
         orders.add(order);
     }
