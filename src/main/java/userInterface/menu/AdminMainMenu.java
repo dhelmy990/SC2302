ee5f4bdc2e84232daa4708de84509e860e55d8c1
sellmenn <105147577+sellmenn@users.noreply.github.com>
2025-04-11 14:23:48 +0800

    update

--- src/main/java/userInterface/menu/AdminMainMenu.java
+++ src/main/java/userInterface/menu/AdminMainMenu.java
@@ -1,20 +1,10 @@
 package userInterface.menu;
 
+
 public class AdminMainMenu implements iMainMenu {
     @Override
     public void display(){
-        System.out.println("\n--- Admin Menu ---");
-        System.out.println("1. Add New User");
-        System.out.println("2. Add New Stall");
-        System.out.println("3. View All Users");
-        System.out.println("4. View All Stalls");
-        System.out.println("5. Edit User Details");
-        System.out.println("6. Edit Stall Name");
-        System.out.println("7. Reassign Stall to New Owner"); 
-        System.out.println("8. Remove User");
-        System.out.println("9. Remove Stall");
-        System.out.println("10. View All Transactions");
-        System.out.println("11. View/Update Account Details");
-        System.out.println("12. Logout");
+        AdminMainMenu adminMainMenu = new AdminMainMenu();
+        adminMainMenu.display();
     }
 }
